38a409da8908c2c1fc48b1da0610e242bf4d2083
evanlarkin10 <evan.larkin@unb.ca>
2020-11-11 16:05:48 -0400

    Changed plot update to 100ms

--- MainWidgets/CSVGraphWidget.py
+++ MainWidgets/CSVGraphWidget.py
@@ -15,6 +15,8 @@ class CSVGraphWidget(QtWidgets.QWidget):
 
         self.graphicsView = pg.PlotWidget()
         self.graphicsView.setYRange(-0.1, 0.1, padding=0)
+        ax = self.graphicsView.getAxis('bottom')  # This is the trick
+        ax.setTickSpacing(1000, 500)
         data = np.genfromtxt("Data/emg1KT60.csv", delimiter=",", names=["x"])
         self.data = data['x']
         self.currentIndex = 1000
@@ -31,18 +33,19 @@ class CSVGraphWidget(QtWidgets.QWidget):
     def handleStart(self):
         print('start')
         self.timer = QtCore.QTimer()
-        self.timer.setInterval(10)
+        self.timer.setInterval(100)
         self.timer.timeout.connect(self.update_plot_data_csv)
         self.timer.start()
 
     def update_plot_data_csv(self):
-        self.x = self.x[1:]  # Remove the first y element.
-        self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
+        self.x = self.x[100:]  # Remove the first 100 elements.
+        nextx =list(range(self.currentIndex, self.currentIndex+100))
+        self.x = np.append(self.x,nextx) # Add a new values.
 
-        self.y = self.y[10:]  # Remove the 10 first
-        newArr = np.append(self.y, self.data[self.currentIndex:self.currentIndex + 10])
+        self.y = self.y[100:]  # Remove the 100 first
+        newArr = np.append(self.y, self.data[self.currentIndex:self.currentIndex + 100])
         self.y = newArr
-        self.currentIndex = self.currentIndex + 10
+        self.currentIndex = self.currentIndex + 100
         self.data_line.setData(self.x, self.y)  # Update the data.
 
     def handleStop(self):
