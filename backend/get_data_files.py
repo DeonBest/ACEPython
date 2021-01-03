d47dfb68ccbb4f9170ca9e6f8feb811a2c197125
evanlarkin10 <evan.larkin@unb.ca>
2021-01-03 17:08:20 -0400

    get csv data

--- backend/get_data_files.py
+++ backend/get_data_files.py
@@ -2,15 +2,18 @@ import requests
 from bs4 import BeautifulSoup as bs
 import sys
 import glob
+import json
 import os
 #how to get args
 #city = sys.argv[1]
-
+#another way to structure
+# if __name__ == '__main__':
+#print(calc(argv[1]))
 
 def get_data_files():
     my_list=[]
     for file in glob.glob(os.path.dirname(os.path.realpath(__file__))+'/data/*.csv'):
         my_list.append(file)
     return my_list
-print(get_data_files())
+print(json.dumps(get_data_files()))
 sys.stdout.flush()
--- frontend/linkers/quickCollect.js
+++ frontend/linkers/quickCollect.js
@@ -1,39 +1,56 @@
 let { PythonShell } = require("python-shell");
 var path = require("path");
 
-function getDataFiles(){
+function getDataFiles() {
   var options = {
     scriptPath: path.join(__dirname, "../../backend/"),
     args: [],
+    mode: "json",
   };
 
   let pyshell = new PythonShell("get_data_files.py", options);
 
-
-  pyshell.on("message", function (message) {
-    //Python returns string with ', must parse string into array with "
-    let data_files = message.replace(/'/g, '"');
-    data_files = JSON.parse(data_files)
-    for(i=0;i<data_files.length;i++){
-      const split = data_files[i].split('/')
-      file_name = split[split.length-1]
-      $('#file-select').append($('<option>', { 
+  pyshell.on("message", function (data_files) {
+    for (i = 0; i < data_files.length; i++) {
+      const split = data_files[i].split("/");
+      file_name = split[split.length - 1];
+      $("#file-select").append(
+        $("<option>", {
           value: data_files[i],
-          text : file_name
-      }));
+          text: file_name,
+        })
+      );
     }
-
   });
 }
 
-function onFileSelect(){
-  //set radio to file and enable the file selector
-  $('#file-select').attr('disabled', false)
-  $('#input-type-live').attr('checked', false)
+function getCSVData() {
+  const file = $("#file-select option:selected").val();
+  console.log("F", file);
+  var options = {
+    scriptPath: path.join(__dirname, "../../backend/"),
+    args: [file],
+    mode: "json",
+  };
+
+  let pyshell = new PythonShell("get_csv_data.py", options);
+
+  pyshell.on("message", function (message) {
+    console.log("M", message);
+    return "test";
+  });
+
+  pyshell.end();
 }
 
-function onLiveSelect(){
+function onFileSelect() {
+  //set radio to file and enable the file selector
+  $("#file-select").attr("disabled", false);
+  $("#input-type-live").attr("checked", false);
+}
+
+function onLiveSelect() {
   //set radio to live and disable the file selector
-  $('#file-select').attr('disabled', true)
-  $('#input-type-file').attr('checked', false)
+  $("#file-select").attr("disabled", true);
+  $("#input-type-file").attr("checked", false);
 }
