ac97d5ab857baf332742986d62e9b3d03e3dcd71
cpsTwiced <sjm9711@hotmail.com>
2021-03-17 15:31:08 -0300

    delsys reading

--- backend/readers/DelsysReader.py
+++ backend/readers/DelsysReader.py
@@ -5,10 +5,12 @@ from readers.Reader import Reader
 import socket
 import sys
 import time
-
+import numpy as np
+from ast import literal_eval
 
 class DelsysReader(Reader):
-    def __init__(self, framesize=100, hostip="localhost", commandport=50040, emgport=50041, imuport=50042):
+    def __init__(self, framesize=100, hostip="localhost", commandport=50048, emgport=50041, imuport=50042):
+        self.framesize = framesize
         self.hostip = hostip
         self.commandport = commandport
         self.emgport = emgport
@@ -16,7 +18,7 @@ class DelsysReader(Reader):
         self.maxSensors = 16
         self.EMGSignal = []
         self.IMUSignal = []
-        self.sensorsPaired = 0
+        self.pairedSensors = 0
         self.activeSensors = 0
         # EMG parameters.
         self.EMGMaxSamplesPerFrame = 26
@@ -56,38 +58,59 @@ class DelsysReader(Reader):
             self.maxSensors * self.EMGChannelCount * 4
         self.bytesToReadIMU = self.IMUNativeSamplesPerFrame * \
             self.maxSensors * self.IMUChannelCount * 4
-
+        self.result=[]
         self.connect()
+        self.start()
+        self.readEMG()
+        self.stop()
+        self.disconnect()
 
     def updateEMGSignal(self, data):
         print("Update EMG", data)
 
+    def readEMG(self):
+        ind = 0
+        while ind<3:
+            data = self.EMGSocket.recv(self.maxSensors * self.EMGChannelCount * 8)
+            arr = np.frombuffer(data, dtype=np.uint8)
+            arr2= arr.astype(np.float64)
+            datashaped = arr.reshape(self.maxSensors * self.EMGChannelCount, -1)
+            print(datashaped)
+            if(ind is not 0):
+                self.result= np.concatenate((self.result, datashaped.tolist()), axis=1)
+            else:
+                self.result = datashaped.tolist()
+            ind= ind + 1
     def read(self, channels):
-        result = []
-        return result
+        print("RESULT", self.result)
+        return self.result.tolist()
 
     def connect(self):
         try:
             print("Connect")
-            test_addr = ('localhost', 80)
-            self.commandSocket = socket.socket()
+
+            self.commandSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.EMGSocket= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.IMUSocket= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 
             server_address_cmd = (self.hostip, self.commandport)
             self.commandSocket.connect(server_address_cmd)
 
             time.sleep(self.baseStationDelay)
+
             # Disregard first message from base station, always Delsys Trigno System Digital Protocol Version x.y.z
-            amount_received = 0
-            while amount_received < 1000:
-                data = commandSocket.recv(16)
-                amount_received += len(data)
-                print('received "%s"' % data)
+            data = self.commandSocket.recv(60)
+            print('received "%s"' % data)
+            print(len(data))
 
             server_address_emg = (self.hostip, self.emgport)
             self.EMGSocket.connect(server_address_emg)
 
             server_address_imu = (self.hostip, self.imuport)
             self.IMUSocket.connect(server_address_imu)
+
+            self.getHardwareInfo()
         except Exception as e:
             print(e)
 
@@ -98,6 +121,7 @@ class DelsysReader(Reader):
         self.EMGSocket = None
         self.IMUSocket.close()
         self.IMUSocket = None
+        print("CLOSED")
 
     # Setting 1 = record on, 0 = record off
     def record(self, setting):
@@ -106,6 +130,82 @@ class DelsysReader(Reader):
     def start(self):
         if(self.commandSocket == None):
             print('No connection be sure to run connect() first')
+        start = "START\r\n\r\n"
+        self.commandSocket.send(start.encode())
+        
+        time.sleep(self.sensorDelay)
+        try:
+            data = self.commandSocket.recv(6).decode('utf-8')
+            print('Start data', data)
+            if 'OK' not in data:
+                print('Unable to stop data collection')
+               
+        except Exception as e:
+            print(e)
 
-        for attempt in range(1, self.sensorMaxRetries):
-            time.sleep(sensorDelay)
+    def stop(self):
+        if(self.commandSocket == None):
+            print('No connection be sure to run connect() first')
+        stop = "STOP\r\n\r\n"
+        self.commandSocket.send(stop.encode())
+        
+        time.sleep(self.sensorDelay)
+        try:
+            data = self.commandSocket.recv(6).decode('utf-8')
+            print('Stop data', data)
+            if 'OK' not in data:
+                print('Unable to stop data collection')
+        except Exception as e:
+            print(e)
+
+    def getHardwareInfo(self):
+        print('test')
+        self.pairedSensors = self.getSensorsPaired()
+        print(self.pairedSensors)
+        self.activeSensors = self.getSensorsActive()
+        print(self.activeSensors)
+
+    def getSensorsPaired(self):
+        result = []
+        for i in range(1, self.maxSensors):
+            msg = "SENSOR %s PAIRED?\r\n\r\n" % str(i)
+            self.commandSocket.send(msg.encode())
+            time.sleep(self.baseStationDelay)
+            
+            try:
+                data = self.commandSocket.recv(7)
+                if 'YES' not in data.decode('utf-8'):
+                    print(i, 'NOT CONNECTED')
+                else: 
+                    print(i, "CONNECTED")
+                    result.append(i)
+                
+            except Exception as e:
+                print('error')
+                print(e)
+        if len(result)==0:
+            return 0
+        else:
+            return result
+
+    def getSensorsActive(self):
+        result = []
+        for i in range(0, len(self.pairedSensors)):
+            sensorId = self.pairedSensors[i]
+            msg = "SENSOR %d ACTIVE?\r\n\r\n" % sensorId
+            self.commandSocket.send(msg.encode())
+            time.sleep(self.sensorDelay)
+            try:
+                data = self.commandSocket.recv(6).decode('utf-8')
+                if 'YES' not in data:
+                    print(sensorId, 'NOT ACTIVE')
+                else: 
+                    print(sensorId, "ACTIVE")
+                    result.append(sensorId)
+                
+            except Exception as e:
+                print(e)
+        if len(result)==0:
+            return 0
+        else:
+            return result
--- backend/testflask.py
+++ backend/testflask.py
@@ -1,7 +1,8 @@
 import requests
 import time
+string = 'testing %d val' % 10
+import numpy as np
 
 print(requests.get('http://127.0.0.1:5000/getreaders').json())
-val = requests.get('http://127.0.0.1:5000/setfileinput/emg1KT60').json()
-val = requests.get('http://127.0.0.1:5000/read/micreader/2').json()
+val = requests.get('http://127.0.0.1:5000/read/delsysreader/2').json()
 print(val)
